9d1894024f5280bcf76e3d62efcc39b855237a5a
RestartFU <github@restartfu.com>
2026-07-18 16:10:54 -0400

    fix(tooling): support enum variant rename
    Resolve qualified enum variant objects without objectpath so rename updates declarations and uses across packages.

--- src/cmd/internal/enumvendor/main.go
+++ src/cmd/internal/enumvendor/main.go
@@ -1,3 +1,7 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Command enumvendor reapplies the first-class enum compatibility changes to
 // the x/tools packages in cmd/vendor after "go mod vendor" replaces them.
 package main
